41d301e55d685cab5e19175345d1795871e65dbc
bevzzz <--global>
2021-08-21 18:12:24 +0200

    refactor: move server startup to /svc dir

--- svc/facerec.py
+++ svc/facerec.py
@@ -1,24 +1,9 @@
-from flask import Flask
-from flask_restx import Api, Resource, fields
-
-app = Flask(__name__)
-api = Api(app)
-
-ns = api.namespace("facerec", description="Recognizing people's faces")
-
-model = api.model("recognize", {
-    'name': fields.String
-})
-
-
-@ns.route('/recognize')
-class RecognizeFace(Resource):
-    def get(self):
-        return "Dima"
-
-    def post(self):
-        return "Vasya"
+from lib.facerec.api import app
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=True
+    )
