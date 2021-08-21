b27f09e43492ee8ed0ad619251bbbaaf3e37def3
bevzzz <--global>
2021-08-21 21:36:55 +0200

    feat: add a basic  model for Person

--- lib/model/person.py
+++ lib/model/person.py
@@ -1,4 +1,7 @@
-
 class Person:
-    def __init__(self, username="unknown"):
+    def __init__(self, username=None):
         self._username = username
+
+    @property
+    def username(self):
+        return self._username
